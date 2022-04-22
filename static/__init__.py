7416e50de130c86c914fe75f40d8e21f8d417573
aprakash7 <aksh.dragon@gmail.com>
2022-04-22 15:05:42 +0530

    changed config

--- static/__init__.py
+++ static/__init__.py
@@ -1,3 +1,4 @@
+from os import wait
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -16,7 +17,7 @@ cors = CORS()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://YOUR_URI'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fkyyecinyjunlp:12fac2481084321e043ba15dc0fc320eab08c009998f99a6c9572154aa5b5c3a@ec2-52-71-69-66.compute-1.amazonaws.com:5432/d1k240fqos8h3h'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from static.todoApp.model.todo_list_model import Todo
     db.init_app(app)
